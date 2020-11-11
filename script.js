1516012e92c54e060d60960cdf62a67fb720b3bd
Barrie Wu <wu.barrie@gmail.com>
2020-11-11 00:22:08 -0500

    Finished HTML & most styling :house::tada:

---
+++ script.js
@@ -0,0 +1,23 @@
+// Get character name from a text input and stor it in a variable
+
+// Change innerHtml of element containing character name
+
+// Replace character name form with a gameplay form that has two radio options
+
+// Create 2 data structures of odds, each with a different array of odds (win:lose ratio) depending on choice
+    // Hard coded odds
+
+// Create a randomizer function to iterate through odds array items and choose a random items
+
+// Event listener on submit to take in userChoice
+    // Prevent default of event listener
+
+
+// STRETCH
+    // Create multiple scenario objects for a deeper gameplay
+
+    // Research a way to make odds more dynamic, instead of hard coded
+
+    // Condintional for if loss occurs, change display of heart values
+
+    // Restart game if hearts empty
